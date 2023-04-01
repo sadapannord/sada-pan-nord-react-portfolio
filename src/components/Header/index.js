809634fc6564437a6d61d6beb340658b5c7b09bc
thenordplace <sadapan@gmail.com>
2023-04-01 12:13:10 -0600

    settled header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 // Import React Router Link component for internal hyperlinks
 import { Link } from "react-router-dom";
+import "materialize-css";
 
 const Header = () => {
   return (
-    <header className="bg-info text-dark mb-4 py-3 display-flex align-center header">
-      <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
+    <header className=" header valign-wrapper">
+      <div className="container text-center valign-wrapper">
         {/* Use Link component to create a link that returns to the homepage on click */}
 
-        <h1 className="m-0" style={{ fontSize: "3rem" }}>
+        <h1 className="m-0 left-align" style={{ fontSize: "3rem" }}>
           Sada Pan-Nord
         </h1>
 
